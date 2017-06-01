1f787b786bfbf1443ea50416866d68527b18db29
pünktchen <epunkt1@freenet.de>
2017-06-01 20:13:02 +0200

    1.5.4.0
    Version bump

--- MediaPortalTVPlugin/Properties/AssemblyInfo.cs
+++ MediaPortalTVPlugin/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MediaBrowser.Plugins.MediaPortal")]
-[assembly: AssemblyCopyright("Copyright © https://github.com/puenktchen 2016")]
+[assembly: AssemblyCopyright("Copyright © https://github.com/puenktchen 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.3.000")]
+[assembly: AssemblyVersion("1.5.4.000")]
 
 //// BUild type
 //#if DEBUG
